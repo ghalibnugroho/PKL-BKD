7c08736577a880521e60e1938d518af20754f502
Ghalib <gholibinf25@gmail.com>
2019-03-30 01:36:30 +0700

    PKL-BKD

---
+++ application/controllers/Auth.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'PKL_INF';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('fullName', 'Full Name', 'required|trim'); //trim untuk menghilangkan spasi agar tdk masuk ke database
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!',
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!',
+        ]); //matches untuk singkronisasi
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]'); //min_length untuk input minimal <-> matches untuk singkronisasi
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'PKL_INF - Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('fullName'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time(),
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulations! Akun anda telah terdaftar. Silahkan Login</div>');
+            redirect('auth');
+        }
+    }
+}
---
+++ application/views/auth/login.php
@@ -0,0 +1,45 @@
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+      <div class="col-8 col-lg-6 my-5">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">PKL Nih</h1>
+                  </div>
+                  <?=$this->session->flashdata('message');?>
+                  <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                      </div>
+                    </div>
+                    <button class="btn btn-primary btn-user btn-block">
+                      Login
+                    </button>
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="auth/registration">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
