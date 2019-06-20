79dfe10970af54b632b9593f71902dc467b4bce6
GH <gholibinf25@gmail.com>
2019-06-20 08:56:44 +0700

    Update displaying chart js
    nampilno chart dummy

--- application/views/templates/auth_footer.php
+++ application/views/templates/auth_footer.php
@@ -7,6 +7,9 @@
 
     <!-- Custom scripts for all pages-->
     <script src="<?=base_url('assets/');?>js/sb-admin-2.min.js"></script>
+    <script src="<?=base_url('assets/');?>vendor/chart.js/Chart.min.js""></script>
+    <script src="<?=base_url('assets/');?>js/demo/chart-area-demo.js"></script>
+    <script src="<?=base_url('assets/');?>js/demo/chart-pie-demo.js"></script>
 
 </body>
 
