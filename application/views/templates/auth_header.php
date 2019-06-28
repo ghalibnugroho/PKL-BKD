fcf304a14ea6c79706fcd24f6d00329fb198601a
GH <gholibinf25@gmail.com>
2019-06-28 08:06:19 +0700

    Fix background color login page

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title><?php echo SITE_NAME ." : ". ucfirst($this->uri->segment(2)) ?></title>
+  <title><?php echo SITE_NAME . " : " . ucfirst($this->uri->segment(2)) ?></title>
 
 
   <!-- Custom fonts for this template-->
@@ -26,3 +26,4 @@
 </head>
 
 
+<body class="bg-gradient-primary">
