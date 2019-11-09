21824d3886572bd092757f56e74f1253e6c24c41
ghalibnugroho <gholibinf25@gmail.com>
2019-11-09 14:30:59 +0700

    Fix Graphic Admin sorting test,

--- application/views/home.php
+++ application/views/home.php
@@ -306,6 +306,7 @@ require_once('templates/session.php');
       });
       chart.update();
     }
+
     // Set new default font family and font color to mimic Bootstrap's default styling
     Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
     Chart.defaults.global.defaultFontColor = '#858796';
