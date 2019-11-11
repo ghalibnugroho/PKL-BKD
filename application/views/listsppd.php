cac653d8cfd492a9611c8f2c4af12fee7a554dab
bregaster <bregasster@gmail.com>
2019-11-11 08:14:26 +0700

    Paging list ST SPPD rincian
    css tidak sinkron

--- application/views/listsppd.php
+++ application/views/listsppd.php
@@ -105,6 +105,9 @@ require_once('templates/session.php');
 
 </html>
 <script>
+  $(document).ready( function () {
+    $('#dataTable').DataTable();
+  } );
   var timeout = 4000; // in miliseconds (3*1000)
 
   $('.alert').delay(timeout).fadeOut(500);
